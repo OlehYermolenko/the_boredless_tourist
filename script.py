1991bc55ef4c68e38df2fdd74cf48fc5b29dbe77
eratheo <yermolenko.oleg@gmail.com>
2020-03-20 11:19:09 -0500

    Added test objects

---
+++ script.py
@@ -0,0 +1,7 @@
+# Create list with destinations
+destinations = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'São Paulo, Brazil', 'Cairo, Egypt']
+
+# Define a test traveler to see how functionality is working so far
+# This is a traveler (a user of the application)
+# Whose name is Erin Wilkes who likes historical buildings and art. Erin is in China right now
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
